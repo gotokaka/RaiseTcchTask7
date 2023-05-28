abcc97bac1f43cfb466a1e822904ee510faa22a4
gotokaka <kakaporider510@gmail.com>
2023-05-28 16:14:30 +0900

    `@PostMapping`アノテーションと新クラスの作成
    -Controllerクラスに @PostMapping("/names")の追加
    -CreateFormクラスの追加。

--- src/main/java/com/example/restapi/NameController.java
+++ src/main/java/com/example/restapi/NameController.java
@@ -1,8 +1,13 @@
 package com.example.restapi;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -13,4 +18,13 @@ public class NameController {
     return List.of("goto", "nakayama", "kinnikun");
   }
 
+  @PostMapping("/names")
+  public ResponseEntity<String> create(@RequestBody CreateForm form) {
+    // 登録処理は省略
+    URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+        .path("/names/id") // id部分は実際に登録された際に発⾏したidを設定する
+        .build()
+        .toUri();
+    return ResponseEntity.created(url).body("name successfully created");
+  }
 }
