e7601a658b2ea54eb14d2607c13c48e9e7299a16
gotokaka <kakaporider510@gmail.com>
2023-06-01 11:22:19 +0900

    @RequestParamの追加
    20-25行目に@RequestParamを追加

--- src/main/java/com/example/restapi/NameController.java
+++ src/main/java/com/example/restapi/NameController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -15,6 +16,12 @@ import java.util.Map;
 
 @RestController
 public class NameController {
+
+  @GetMapping("/hello")
+  public String hello(
+      @RequestParam(value = "name", defaultValue = "kakapo") String name) {
+    return String.format("<h1>Hello %s!</h1>", name);
+  }
 
   @GetMapping("/names")
   public List<String> getNames() {
